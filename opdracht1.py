1fa9d464ca163a802577ffff2df4f4a550f542d0
ysbrandm11 <ysbrandm@xs4all.nl>
2022-03-16 09:48:01 +0100

    Je moeder

--- opdracht1.py
+++ opdracht1.py
@@ -8,6 +8,8 @@ def rot2d(a, b):
     return np.multiply(matrix, factor)
 
 # COMMENT
+
+
 def approx_svd(A, j):
     if j == 0:
         return A
@@ -16,33 +18,62 @@ def approx_svd(A, j):
         return rot2d(prevA[0][0], prevA[1][0]) @ prevA
     return prevA @ rot2d(prevA[0][0], prevA[1][0])
 
+
 def sigma(a):
     return np.linalg.svd(a)
 
-def rot(n,a,b,i):
+
+def rot(n, a, b, i):
     boven = np.eye(i)
     onder = np.eye(n-i-2)
     return scipy.linalg.block_diag(boven, rot2d(a, b), onder)
 
+
 def nul_links(A, i, j):
-    a=A[i-2][j-1]
-    b=A[i-1][j-1]
+    a = A[i-2][j-1]
+    b = A[i-1][j-1]
     return rot(len(A), a, b, (i-2))
 
-def rot_rechts(n,a,b,i):
+
+def rot_rechts(n, a, b, i):
     boven = np.eye(i)
     onder = np.eye(n-i-2)
     return scipy.linalg.block_diag(boven, rot2d(a, b).T, onder)
 
+
 def nul_rechts(A, i, j):
-    c=A[i-1][j-2]
-    d=A[i-1][j-1]
+    c = A[i-1][j-2]
+    d = A[i-1][j-1]
     return rot_rechts(len(A), c, d, (i-1))
 
-test = np.array(((1, 2),(2, 3)))
 
-tessst = nul_rechts(np.array(((1,2,3,4,4),(4,3,2,1,2),(3,2,1,3,5),(3,4,7,4,3),(3,4,5,3,2))), 1, 2)
+def bidiagonaliseer(A):
+    matrix = A
+    for i in range(2, len(A)+1)[::-1]:
+        matrix = nul_links(matrix, i, 1)@matrix
+    # print(matrix)
+    #print(nul_rechts(matrix, 1, 2))
+    #matrix = matrix@nul_rechts(matrix, 1, 2)
+    for i in range(3, len(A)+1)[::-1]:
+        matrix = nul_links(matrix, i, 2)@matrix
+    matrix = nul_links(matrix, 4, 3)@matrix
+    return matrix
 
-mattie = np.array(((1,2,3,4,4),(4,3,2,1,2),(3,2,1,3,5),(3,4,7,4,3),(3,4,5,3,2)))
 
-print(mattie@tessst)
+test = np.array(((1, 2), (2, 3)))
+
+tessst = nul_rechts(np.array(((1, 2, 3, 4, 4), (4, 3, 2, 1, 2),
+                    (3, 2, 1, 3, 5), (3, 4, 7, 4, 3), (3, 4, 5, 3, 2))), 1, 2)
+
+mattie = np.array(((1, 2, 3, 4, 4), (4, 3, 2, 1, 2),
+                  (3, 2, 1, 3, 5), (3, 4, 7, 4, 3), (3, 4, 5, 3, 2)))
+
+randommatrix = np.random.rand(4, 3)
+
+print(bidiagonaliseer(randommatrix))
+
+
+# print(mattie@tessst)
+
+# Voor nul links werken alleen waardes in de onderdriehoek, voor nul rechts
+# alleen bovendriehoeks. En voor beide werkt de diagonaal niet.
